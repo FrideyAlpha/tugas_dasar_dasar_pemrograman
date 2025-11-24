e232ae2aa9c983f55815aa50b8096562fb0bd2f9
Fridey Alphaisra Akbar <fridey.alpha@gmail.com>
2025-11-24 18:48:08 +0700

    Refactor code structure for improved readability and maintainability

--- tugas_nomor1.c
+++ tugas_nomor1.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    float suhuCel, reamur, farenheit;
+    float suhuCel, reamur, fahrenheit;
 
-    printf("Program Konversi Suhu Celsius ke Reamur dan Farenheit\n");
+    printf("Program Konversi Suhu Celsius ke Reamur dan Fahrenheit\n");
     printf("------------------------------------------------------\n");
 
     printf("Masukkan suhu dalam Celsius : ");
     scanf("%f", &suhuCel);
 
     reamur = suhuCel * (4.0/5.0);
-    farenheit = (suhuCel * 9.0/5.0) + 32;
+    fahrenheit = (suhuCel * 9.0/5.0) + 32;
 
     printf("\nHasil Konversi:\n");
     printf("Reamur     : %.2f\n", reamur);
-    printf("Farenheit  : %.2f\n", farenheit);
+    printf("fahrenheit  : %.2f\n", fahrenheit);
 
     return 0;
 }
--- tugas_nomor1.pas
+++ tugas_nomor1.pas
@@ -3,25 +3,23 @@ program KonversiSuhu;
 uses crt;
 
 var
-  suhuCel : real;
-  reamur  : real;
-  farenheit : real;
+  suhuCel, reamur, fahrenheit : real;
 
 begin
   clrscr;
 
-  writeln('Program Konversi Suhu Celsius ke Reamur dan Farenheit');
+  writeln('Program Konversi Suhu Celsius ke Reamur dan Fahrenheit');
   writeln('------------------------------------------------------');
   write('Masukkan suhu dalam Celsius : ');
   readln(suhuCel);
 
   reamur := suhuCel * (4/5);
-  farenheit := (suhuCel * 9/5) + 32;
+  fahrenheit := (suhuCel * 9/5) + 32;
 
   writeln;
   writeln('Hasil Konversi:');
   writeln('Reamur     : ', reamur:0:2);
-  writeln('Farenheit  : ', farenheit:0:2);
+  writeln('fahrenheit  : ', fahrenheit:0:2);
 
   readln;
 end.
